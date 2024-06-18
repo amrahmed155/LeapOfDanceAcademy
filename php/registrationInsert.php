2f9adfc4182c6523b0583995072f71ba2b1f7e51
amrahmed155 <54851931+amrahmed155@users.noreply.github.com>
2024-06-18 10:41:52 +0300

    registration added

---
+++ php/registrationInsert.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost"; // change if necessary
+$username = "Amr"; // change if necessary
+$password = "amr@2020"; // change if necessary
+$dbname = "leapofdanceacademy"; // change to your database name
+
+
+// Set parameters and execute
+$name = $_POST['Name'];
+$DOB = $_POST['DOB'];
+$gender = $_POST['gender'];
+$address = $_POST['streetAddress'];
+$parentName = $_POST['parentName'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$status = $_POST['status'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO `register` (`id`, `name`, `gender`, `address`, `parentName`, `email`, `phone`, `status`, `DOB`) 
+VALUES (NULL, '$name', '$gender', '$address', '$parentName', '$email', '$phone',$status,'$DOB')"; 
+// VALUES ('$interestReason', '$stayingDuration', '$skills', $GoFundMeRadio, '$GoFundMeOtherReason', '$ideas', '$email', '$phone', $status)";
+
+if ($conn->query($sql) === TRUE) {
+    print json_encode(['status' => 'success', 'message' => 'New record created successfully']);
+} else {
+    print json_encode(['status' => 'error', 'message' => 'Error: ' . $stmt->error]);
+}
+
+$conn->close();
+?> 
